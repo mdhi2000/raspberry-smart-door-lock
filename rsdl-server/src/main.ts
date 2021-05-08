d8a0de1a39afa6f9b5aeb04015bf5a0d572f3a02
Mohammad Habibi <mdhi2000@gmail.com>
2021-05-08 19:30:29 +0430

    added microservice connect to main.ts

--- rsdl-server/src/main.ts
+++ rsdl-server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { WsAdapter } from './adapters/web-socket.adapter';
 // import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({});
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(8000);
 }
